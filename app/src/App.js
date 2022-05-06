220ef191333c787777f49e06c5801f522be36009
iIngashev <isaingashev@gmail.com>
2022-05-06 18:12:08 +0300

    added watched component

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 import {Header} from "./components/Header";
 import {Watchlist} from "./components/Watchlist";
+import {Watched} from "./components/Watched";
 import {Add} from "./components/Add";
 import {GlobalProvider} from "./context/GlobalState";
 
@@ -17,6 +18,7 @@ function App() {
                     <Header/>
                     <Routes>
                         <Route exact path="/" element={<Watchlist/>}/>
+                        <Route exact path="/watched" element={<Watched/>}/>
                         <Route exact path="/add" element={<Add/>}/>
                     </Routes>
                 </BrowserRouter>
