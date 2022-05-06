bc4459e6d3fcc0a56e7a3039f344721a33471e99
iIngashev <isaingashev@gmail.com>
2022-05-06 18:02:06 +0300

    added header with

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+
+import {Header} from "./components/Header";
+import {Watchlist} from "./components/Watchlist";
+
 import './App.css';
 
 function App() {
   return (
     <>
-
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Watchlist/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
---
+++ app/src/components/MovieControls.js
@@ -0,0 +1,37 @@
+import React, {useContext} from "react";
+import {GlobalContext} from "../context/GlobalState";
+
+export const MovieControls = ({movie, type}) => {
+    const {
+        removeMovieFromWatchlist,
+        addMovieToWatched,
+        moveToWatchlist,
+        removeFromWatched
+    } = useContext(GlobalContext)
+
+    return (
+        <div className="inner-card-controls">
+            {type === 'watchlist' && (
+                <>
+                    <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+                        <i className="fa-fw far fa-eye"/>
+                    </button>
+
+                    <button className="ctrl-btn" onClick={() => removeMovieFromWatchlist(movie.id)}>
+                        <i className="fa-fw fa fa-times"/>
+                    </button>
+                </>
+            )}
+            {type === 'watched' &&
+            <>
+                <button className="ctrl-btn" onClick={() => moveToWatchlist(movie)}>
+                    <i className="fa-fw far fa-eye-slash"/>
+                </button>
+                <button className="ctrl-btn" onClick={() => removeFromWatched(movie.id)}>
+                    <i className="fa-fw fa fa-times"/>
+                </button>
+            </>
+            }
+        </div>
+    )
+}
